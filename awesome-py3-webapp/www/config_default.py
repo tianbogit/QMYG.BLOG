1495711c1a4abcff2ab3d3da60d70e3eb661c927
tianpo <mailtp@foxmail.com>
2018-09-14 18:06:03 +0800

    处理request

---
+++ awesome-py3-webapp/www/config_default.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/14 17:56
+# @Author  : TIANPO
+# @Email   : mailtp@foxmail.com
+# @File    : config_default.py.py
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www',
+        'password': 'www',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ awesome-py3-webapp/www/config_override.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/14 17:57
+# @Author  : TIANPO
+# @Email   : mailtp@foxmail.com
+# @File    : config_override.py.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
